f4b69ff742e35fe0549ff9c90e154ac9aa72a838
Kate <Riba0017@yandex.ru>
2021-01-29 15:11:20 +0300

    feat: add header and footer to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 // components
+import Header from "./components/Header/Header"
 import Timer from "./components/Timer/Timer";
 import MainWrapper from "./components/Wrappers/MainWrapper";
+import Footer from "./components/Footer/Footer"
 // styles
 import 'antd/dist/antd.css';
 
 function App() {
   return (
-    <MainWrapper>
-      <Timer />
-    </MainWrapper>
+    <>
+      <Header/>
+      <MainWrapper>
+         <Timer />
+       </MainWrapper>
+       <Footer/>
+    </>
   );
 }
 
