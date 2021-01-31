249ef807add2643f1be6d0fade19bb8598be157e
Chekhuta Dmytro <chehuta1996@gmail.com>
2021-01-31 13:06:33 +0200

    feat: implement SET_TASK_NAME action to Timer component

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,20 +1,38 @@
 import React from "react";
+import { connect } from "react-redux";
 // components
 import MainWrapper from "../Wrappers/MainWrapper";
 // antd-components
 import { Input } from "antd";
 import { Button } from "antd";
+// actions
+import { setTaskName } from "../../store/actions/currentTaskActions";
 // styles
 import "./styles.css";
 
-const Timer = () => {
+const Timer = ({ currentTask, setTaskName }: any) => {
+  const { taskName, timeSpend, timeStart: activeTimer } = currentTask;
+
   return (
     <MainWrapper>
-      <Input placeholder="Task Name" />
+      <Input
+        placeholder="Task Name"
+        value={taskName}
+        onChange={(e) => setTaskName(e.target.value)}
+      />
       <div className="circle-wrapper">00:00:00</div>
       <Button type="primary">Start</Button>
     </MainWrapper>
   );
 };
 
-export default Timer;
+const mapStateToProps = (state: any) => {
+  return {
+    currentTask: state.currentTask,
+  };
+};
+const mapDispatchToProps = {
+  setTaskName,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Timer);
