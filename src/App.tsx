c0d448cacb13dc82797f541463c76fce216460b4
Chekhuta Dmytro <chehuta1996@gmail.com>
2021-01-31 13:04:57 +0200

    refactor: format App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 // components
-import Header from "./components/Header/Header"
+import Header from "./components/Header/Header";
 import Timer from "./components/Timer/Timer";
 import MainWrapper from "./components/Wrappers/MainWrapper";
-import Footer from "./components/Footer/Footer"
+import Footer from "./components/Footer/Footer";
 // styles
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 
 function App() {
   return (
     <>
-      <Header/>
+      <Header />
       <MainWrapper>
-         <Timer />
-       </MainWrapper>
-       <Footer/>
+        <Timer />
+      </MainWrapper>
+      <Footer />
     </>
   );
 }
